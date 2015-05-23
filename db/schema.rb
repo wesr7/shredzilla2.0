1444da4eeb224b3a3802f99ac151f8601e7f5d63
wesr7 <jwesleyrodgers@gmail.com>
2015-05-23 12:25:01 -0700

    changed workouts model

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522044053) do
+ActiveRecord::Schema.define(version: 20150523001837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "days", force: :cascade do |t|
+    t.integer  "number"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "exercises", force: :cascade do |t|
     t.string   "video"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.integer  "workout_id"
   end
 
   create_table "identities", force: :cascade do |t|
@@ -72,10 +80,10 @@ ActiveRecord::Schema.define(version: 20150522044053) do
     t.string   "hold"
     t.string   "reps"
     t.string   "sets"
-    t.integer  "exercise_id"
-    t.integer  "day_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "day"
+    t.string   "day_name"
   end
 
   add_foreign_key "identities", "users"
