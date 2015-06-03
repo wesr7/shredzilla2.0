84b06ca49712e899fa6b49087aa493fc3ad8e753
wesr7 <jwesleyrodgers@gmail.com>
2015-06-02 19:41:44 -0700

    heroku scheduler working

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -13,4 +13,10 @@
 class Challenge < ActiveRecord::Base
   has_and_belongs_to_many :programs
   has_many :teams
+
+  def self.incrementDay
+    @current_day = Challenge.last.current_day
+    @current_day += 1
+  end
+
 end
