e42ea4e82bb95a997d142fd4ae57df81e8f8eabd
David Lee <david@davidlee.io>
2015-06-04 19:29:31 -0700

    dashboard wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,6 +17,7 @@ class PagesController < ApplicationController
   def dashboard
     skip_authorization
     @teams = Team.all
+
     if current_user.teams != []
       @program = current_user.teams.first.program
       @day = @program.days.where(day: Challenge.first.current_day).first
