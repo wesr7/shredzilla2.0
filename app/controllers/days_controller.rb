b670f804bc15b3b3f7f0241efabe273e0a518a47
wesr7 <jwesleyrodgers@gmail.com>
2015-08-03 23:33:32 -0700

    add day new and create action along with day new view

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -4,8 +4,27 @@ class DaysController < ApplicationController
     @days = Day.all
   end
 
+  def new
+    @day = Day.new
+  end
+
+  def create
+    @day = Day.new(day_params)
+    if @day.save
+        redirect_to days_path(@day)
+    else
+        render "new"
+    end
+  end
+
   def show
     @day = Day.find(params[:id])
   end
+
+  private
+  def day_params
+    params.require(:day).permit(:program_id, :day, :description, :option_1, :option_2)
+  end
+
 
 end
